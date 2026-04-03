c0e24810c7bd5b5aadc328a213f455b0169859d2
Aadarsh Lekhwani <aadarshlekhwani@Aadarshs-MacBook-Pro.local>
2026-04-03 21:07:34 +0530

    finall

--- script.js
+++ script.js
@@ -34,3 +34,20 @@ items.forEach(item => {
     item.classList.toggle("active");
   });
 });
+
+
+
+const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+nav.classList.toggle("active");
+});
+
+
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+link.addEventListener("click", () => {
+nav.classList.remove("active");
+});
+});
