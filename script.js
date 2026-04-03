a9f6a57493cde8132add2ecedff7c6200e865e30
Aadarsh Lekhwani <aadarshlekhwani@Aadarshs-MacBook-Pro.local>
2026-04-03 17:02:44 +0530

    adding drop down categories

---
+++ script.js
@@ -0,0 +1,36 @@
+// MenuMint Category Filter
+
+const tabs = document.querySelectorAll(".tab-btn");
+const items = document.querySelectorAll(".menu-item");
+const categoryTitle = document.getElementById("category-title");
+
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+
+    tabs.forEach(btn => btn.classList.remove("active"));
+    tab.classList.add("active");
+
+    const category = tab.dataset.category;
+
+    // Update category title
+    categoryTitle.textContent = tab.textContent;
+
+    items.forEach(item => {
+      if(category === "all" || item.dataset.category === category){
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    });
+
+  });
+});
+
+
+// Expand Menu Items
+
+items.forEach(item => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("active");
+  });
+});
